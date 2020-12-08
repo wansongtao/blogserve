c50be407d4e495eb4214d0c133bd0b82d26ba82f
万松涛 <2294215581@qq.com>
2020-12-08 18:07:35 +0800

    项目初始化

---
+++ src/index.js
@@ -0,0 +1,60 @@
+/**
+ * @description 主程序文件
+ * @author wansongtao
+ * @date 2020-12-08
+ */
+class Index {
+  constructor () {
+    //   引入模块
+    const express = require('express')
+    const bodyParser = require('body-parser')
+    const router = require('./router/router')
+
+    // 创建express实例
+    const webApp = express()
+
+    // 设置服务器端口
+    webApp.listen(6666, (err) => {
+      if (err) {
+        console.error(err)
+      }
+    })
+
+    // 设置接收数据的格式，表单数据和json格式的都行
+    webApp.use(bodyParser.urlencoded({ extended: false }))
+    webApp.use(bodyParser.json())
+
+    webApp.all('*', (req, res, next) => {
+      // 允许跨域
+      res.header('Access-Control-Allow-Origin', '*')
+      // 设置请求头
+      res.header('Access-Control-Allow-Headers, Content-Type, Authorization')
+
+      next()
+    })
+
+    // 注册路由实例
+    webApp.use(router)
+
+    // 服务器错误处理
+    webApp.use((err, req, res, next) => {
+      console.error('server error: ', err)
+
+      res.status(500).send({
+        statusCode: 400,
+        message: '服务器错误'
+      })
+    })
+
+    // 路径错误处理
+    webApp.use((req, res) => {
+      res.status(404).send({
+        statusCode: 404,
+        message: '路径错误，找不到资源'
+      })
+    })
+  }
+}
+
+// eslint-disable-next-line no-new
+new Index()
---
+++ src/router/router.js
@@ -0,0 +1,29 @@
+/**
+ * @description 路由模块
+ * @author wansongtao
+ * @date 2020-12-08
+ */
+class Router {
+  constructor () {
+    //   引入模块
+    const express = require('express')
+    const path = require('path')
+
+    // 创建路由实例
+    this.myRouter = express.Router()
+
+    // 返回静态资源
+    this.myRouter.use('/upload', express.static(path.join(__dirname, '../upload')))
+  }
+
+  /**
+   * @description 返回路由实例
+   */
+  backRouter () {
+    return this.myRouter
+  }
+}
+
+const ROUTERS = new Router()
+
+module.exports = ROUTERS.backRouter()
